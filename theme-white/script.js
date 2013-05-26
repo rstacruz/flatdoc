613ee8ae79f7e8cfe9405c8d5afaac30918b5dd6
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-26 18:32:09 +0800

    Scrollstick.

--- theme-white/script.js
+++ theme-white/script.js
@@ -1,2 +1,15 @@
 (function($) {
-})();
+  var $window = $(window);
+
+  $(function() {
+    var $sidebar = $('.sidebar');
+    var elY = $sidebar.offset().top;
+
+    $window.on('scroll.sidestick', function() {
+      var scrollY = $window.scrollTop();
+      $sidebar.toggleClass('fixed', (scrollY >= elY));
+    });
+
+    $window.trigger('scroll.sidestick');
+  });
+})(jQuery);
