71e3b08dcdd52a8cb901c06d0ff80668935d720d
Andreas <me@grml.in>
2013-07-24 19:49:27 +0200

    fix sidebar position when resizing

--- theme-white/script.js
+++ theme-white/script.js
@@ -56,21 +56,22 @@
    * Sidebar stick.
    */
 
-  $(function() {
-    var $sidebar = $('.menubar');
-    var elTop;
+    $(function () {
+      var $sidebar = $('.menubar'),
+        elTop;
 
-    $window
-      .on('resize.sidestick', function() {
-        elTop = $sidebar.offset().top;
-        $window.trigger('scroll.sidestick');
-      })
-      .on('scroll.sidestick', function() {
-        var scrollY = $window.scrollTop();
-        $sidebar.toggleClass('fixed', (scrollY >= elTop));
-      })
-      .trigger('resize.sidestick');
-  });
+      $window
+        .on('resize.sidestick', function () {
+          $sidebar.removeClass('fixed');
+          elTop = $sidebar.offset().top;
+          $window.trigger('scroll.sidestick');
+        })
+        .on('scroll.sidestick', function () {
+          var scrollY = $window.scrollTop();
+          $sidebar.toggleClass('fixed', (scrollY >= elTop));
+        })
+        .trigger('resize.sidestick');
+    });
 
 })(jQuery);
 /*! jQuery.scrollagent (c) 2012, Rico Sta. Cruz. MIT License.
